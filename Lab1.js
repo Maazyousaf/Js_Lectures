f5689279e5ca01c7f125c8fca2c288cf501edc8c
maaz.Ali <maaz@renesistech.com>
2023-02-02 11:27:23 +0500

    add

---
+++ Lab1.js
@@ -0,0 +1,57 @@
+// <script>
+// Q1: Js is client side or server side?
+//     Js is client side .
+
+// Q2: Does Js Produce object code?
+//     No js note produced object code or source code its directily run bye web browser 
+
+// Q3: List Down All Data types?
+//     1:Primitive
+//        Bolean
+//        Null
+//        Number
+//        String
+//        Undefine
+//        Symbol
+//     2:NonPrimitive
+//       Object
+
+// Q4: In how many ways a variable can be declare?
+//     Foure ways to declear variables
+//      i: var
+//      ii: Let
+//      iii: const 
+//      vi: Nothing
+
+// Q5: What does a variable scope mean?
+//     Thre are two types of scope 
+//     global scope
+//       in this scope declear varibale globaly 
+//       we use this type of variables in whole file although in functions statments and outsides of functions
+//     Local or Functional scope
+//       in this csope we decleare aa variable inside blocks and only use it inside that function or block
+//       its not accessable out side the function
+//    block scope
+//       if we declear a varible in conditional structure in this case var is accessable inside the block
+//       if condition true then also returs its value but if false not return value
+//       but let or cost are not accessable inside the condition block 
+      
+
+// Q6: code
+//     name new name
+//     inside if new name
+//     its new name new name
+
+// Q7:  code 
+//      name new name
+//      linside if new name
+//      Error message Not define newName
+
+// Q8: code 
+//     name new name
+//     linside if new name
+//     Error message Not define newName
+      
+
+
+// </script>;
